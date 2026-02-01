9ad32e502e9eb58fbcd604d665e02abff8773b3b
ipsitaprusty08 <prustyipsita2004@gmail.com>
2026-02-01 19:38:44 +0530

    Set flash messages to null if undefined

--- app.js
+++ app.js
@@ -69,8 +69,8 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req,res,next)=>{
-  res.locals.success=req.flash("success");
-  res.locals.error=req.flash("error");
+  res.locals.success=req.flash("success") || null;
+  res.locals.error=req.flash("error") || null;
   res.locals.currUser=req.user || null;
   next();
 });
